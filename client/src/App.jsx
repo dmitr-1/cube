afb8bcc340e51ddb3128f65ec45024ff6bcc4532
dmitr-1 <Vzhigd@yandex.ru>
2024-06-13 21:42:27 +0700

    ff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
-import './App.scss';
 import Navbar from './components/navbar/Navbar';
+import './App.scss';
 
 function App() {
   return (
